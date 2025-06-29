a87b0b2c08462f7125b564ee5e082e5a70539637
Abdifatah 2024 <abdi1254@gmail.com>
2025-06-30 00:01:21 +0300

    Fix AppDispatch typing for dispatch

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,41 @@
+// import { useEffect } from "react";
+// import { useDispatch, useSelector } from "react-redux";
+// import { RouterProvider } from "react-router-dom";
+// import { router } from "./routes";
+// import { checkAuth } from "./Redux/auth/LoginSlice";
+// import type { RootState } from "./Redux/store";
+
+// const App = () => {
+//   const dispatch = useDispatch();
+
+//   const { loading } = useSelector((state: RootState) => state.loginSlice);
+
+//   useEffect(() => {
+//     dispatch(checkAuth());
+//   }, [dispatch]);
+
+//   if (loading) {
+//     return (
+//       <div className="flex items-center justify-center min-h-screen">
+//         <p className="text-lg font-semibold">Checking authentication...</p>
+//       </div>
+//     );
+//   }
+
+//   return <RouterProvider router={router} />;
+// };
+
+// export default App;
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes";
 import { checkAuth } from "./Redux/auth/LoginSlice";
-import type { RootState } from "./Redux/store";
+import type { RootState, AppDispatch } from "./Redux/store";
 
 const App = () => {
-  const dispatch = useDispatch();
-
+  // ✅ Correctly type dispatch to allow thunks
+  const dispatch = useDispatch<AppDispatch>();
   const { loading } = useSelector((state: RootState) => state.loginSlice);
 
   useEffect(() => {
